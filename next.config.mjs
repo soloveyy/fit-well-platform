b350b774d2d58ed140f3adbbeaedac04985cd067
den <solovyov.dd@gmail.com>
2024-11-04 11:43:07 +0100

    Change next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ const isDev = process.env.NODE_ENV === 'development';
 
 const nextConfig = {
     basePath: isDev? '' : '/fitwell',
-    assetPrefix: isDev ? '' : '/fitwell'
 };
 
 export default nextConfig;
